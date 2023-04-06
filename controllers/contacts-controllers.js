60d8c687acb7762f6327e85c1e79a26d741c47ee
max-ignat <cadillac.maxim@gmail.com>
2023-04-06 17:52:05 +0300

    add Joi schema to main model

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,60 +1,53 @@
-const { ctrlWrapper ,HttpError } = require("../helpers");
-const contacts = require("../models/contact");
+// const mongoose = require("mongoose");
+const { ctrlWrapper, HttpError } = require("../helpers");
+// const contacts = require("../models/contact");
+const {Contact} = require("../models/contact");
 
+const getAllContontacts = async (req, res) => {
+  const result = await Contact.find();
+  res.json(result);
+};
 
+const getContactById =  async (req, res, ) => {
 
-const getAllContontacts =async (req, res, ) => {
-
-        const result = await contacts.listContacts();
+        const { id } = req.params;
+        const result = await Contact.findById(id);
+        if (!result) {
+          throw HttpError(404);
+        }
         res.json(result);
-     
+
     };
 
+const addContact = async (req, res, ) => {
+        const result = await Contact.create(req.body);
+        res.status(201).json(result);
 
-// const getContactById =  async (req, res, ) => {
-      
-//         const { id } = req.params;
-//         const result = await contacts.getContactById(id);
-//         if (!result) {
-//           throw HttpError(404);
-//         }
-//         res.json(result);
-      
-//     };
+    };
 
+const updateContactById = async (req, res, ) => {
 
-// const addContact = async (req, res, ) => {
-//         const result = await contacts.addContact(req.body);
-//         res.status(201).json(result);
-      
-//     };
+        const { id } = req.params;
+        const result = await Contact.updateContacts(id, req.body);
+        res.status(200).json(result);
 
+    };
 
-// const updateContactById = async (req, res, ) => {
-      
-//         const { id } = req.params;
-//         const result = await contacts.updateContacts(id, req.body);
-//         res.status(200).json(result);
-      
-//     };
+const deleteContactById = async (req, res, ) => {
 
+        const { id } = req.params;
+        const result = await Contact.removeContact(id);
+        if (!result) {
+          throw HttpError(404);
+        }
+        res.json({ message: "contact deleted" });
 
-// const deleteContactById = async (req, res, ) => {
-      
-//         const { id } = req.params;
-//         const result = await contacts.removeContact(id);
-//         if (!result) {
-//           throw HttpError(404);
-//         }
-//         res.json({ message: "contact deleted" });
-      
-//     };
-
+    };
 
 module.exports = {
   getAllContontacts: ctrlWrapper(getAllContontacts),
-  // getContactById: ctrlWrapper(getContactById),
-  // addContact: ctrlWrapper(addContact),
-  // updateContactById: ctrlWrapper(updateContactById),
-  // deleteContactById: ctrlWrapper(deleteContactById),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  updateContactById: ctrlWrapper(updateContactById),
+  deleteContactById: ctrlWrapper(deleteContactById),
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const { validateBody } = require('../../helpers')
-const schema = require('../../schema/contactSchema')
+const schema = require('../../models/contact')
 const router = express.Router();
 const ctrl = require('../../controllers/contacts-controllers')
 
 router.get("/", ctrl.getAllContontacts);
 
-// router.get("/:id", ctrl.getContactById);
+router.get("/:id", ctrl.getContactById);
 
-// router.post("/", validateBody(schema.contactSchema) ,ctrl.addContact);
-// router.put("/:id",validateBody(schema.contactSchema), ctrl.updateContactById);
+router.post("/", validateBody(schema.contactAddSchema), ctrl.addContact);
 
-// router.delete("/:id", ctrl.deleteContactById);
+router.put("/:id",validateBody(schema.contactAddSchema), ctrl.updateContactById);
+
+router.delete("/:id", ctrl.deleteContactById);
 
 module.exports = router;
